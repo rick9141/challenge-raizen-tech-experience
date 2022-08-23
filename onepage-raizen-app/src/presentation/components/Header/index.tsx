c877de98bd578b745674ef93f1193fd3a7d3defd
Luis Henrique Gavirate <gavirate.henrique@gmail.com>
2022-08-23 17:25:23 -0300

    feat: homepage primary

--- onepage-raizen-app/src/presentation/components/Header/index.tsx
+++ onepage-raizen-app/src/presentation/components/Header/index.tsx
@@ -1,9 +1,154 @@
+import { SVGProps } from 'react'
+
+const footerNavigation = {
+  social: [
+    {
+      name: 'Linkedin',
+      href: 'https://www.linkedin.com/in/gavirate/',
+      icon: (props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) => (
+        <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
+          <path
+            fillRule="evenodd"
+            d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-2 8c0 .557-.447 1.008-1 1.008s-1-.45-1-1.008c0-.557.447-1.008 1-1.008s1 .452 1 1.008zm0 2h-2v6h2v-6zm3 0h-2v6h2v-2.861c0-1.722 2.002-1.881 2.002 0v2.861h1.998v-3.359c0-3.284-3.128-3.164-4-1.548v-1.093z 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759zM12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
+            clipRule="evenodd"
+          />
+        </svg>
+      ),
+    },
+    {
+      name: 'GitHub',
+      href: 'https://github.com/rick9141',
+      icon: (props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) => (
+        <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
+          <path
+            fillRule="evenodd"
+            d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
+            clipRule="evenodd"
+          />
+        </svg>
+      ),
+    },
+  ],
+}
+
 function Header() {
   return (
-    <header>
-      <div>Initial structure 1</div>
-    </header>
+    <div className="bg-white">
+      <div className="relative overflow-hidden">
+        <main>
+          <div className="pt-10 bg-gray-900 sm:pt-16 lg:pt-8 lg:pb-14 lg:overflow-hidden">
+            <div className="mx-auto max-w-7xl lg:px-8">
+              <div className="lg:grid lg:grid-cols-2 lg:gap-8">
+                <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 sm:text-center lg:px-0 lg:text-left lg:flex lg:items-center">
+                  <div className="lg:py-24">
+                    <h1 className="mt-4 text-4xl tracking-tight font-bold text-white sm:mt-5 sm:text-6xl sm:tracking-tight lg:mt-6 xl:text-6xl xl:tracking-tight">
+                      <span className="block">Olá, meu nome é Luis</span>
+                      <span className="pb-3 block bg-clip-text text-transparent bg-gradient-to-r from-teal-200 to-cyan-400 sm:pb-5">
+                        Valores & Cultura Importam
+                      </span>
+                    </h1>
+                    <p className="text-base text-gray-300 sm:text-xl lg:text-lg xl:text-xl">
+                      Abaixo falo um pouco sobre cada valor e uma frase do que cada um pode representar ao meus olhos e como os enchergo do meu ponto de vista.
+                    </p>
+                    <div className="mt-10 sm:mt-12">
+                    </div>
+                  </div>
+                </div>
+                <div className="mt-12 -mb-16 sm:-mb-48 lg:m-0 lg:relative">
+                  <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 lg:max-w-none lg:px-0">
+                    <img
+                      className="w-full lg:absolute lg:inset-y-0 lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
+                      src="https://tailwindui.com/img/component-images/cloud-illustration-teal-cyan.svg"
+                      alt=""
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="pb-16 bg-gradient-to-r from-teal-500 to-cyan-600 lg:pb-0 lg:z-10 lg:relative">
+            <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-3 lg:gap-8">
+              <div className="relative lg:-my-8">
+                <div aria-hidden="true" className="absolute inset-x-0 top-0 h-1/2 bg-white lg:hidden" />
+                <div className="mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:p-0 lg:h-full">
+                  <div className="aspect-w-10 aspect-h-6 rounded-xl shadow-xl overflow-hidden sm:aspect-w-16 sm:aspect-h-7 lg:aspect-none lg:h-full">
+                    <img
+                      className="object-cover lg:h-full lg:w-full"
+                      src="../public/aboutme.jpg"
+                      alt=""
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="mt-12 lg:m-0 lg:col-span-2 lg:pl-8">
+                <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 lg:px-0 lg:py-20 lg:max-w-none">
+                  <blockquote>
+                    <div>
+                      <svg
+                        className="h-10 w-10 text-white opacity-25"
+                        fill="currentColor"
+                        viewBox="0 0 32 32"
+                        aria-hidden="true"
+                      >
+                        <path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
+                      </svg>
+                      <p className="mt-6 text-2xl font-medium text-white">
+                        R - Obter uma visão de um angulo diferente não diminui você, mas sim gera maior valor a sua junto a maiores aprendizados.
+                      </p>
+                      <p className="mt-6 text-2xl font-medium text-white">
+                        A - O ponto de vista, depende da vista, do lado em que se olha, do ponto que se olha, do momento que se olha e de cada vista.
+                      </p>
+                      <p className="mt-6 text-2xl font-medium text-white">
+                        I - Seja um questionador da vida, questionamentos fazem parte do desenvolvimento, experiências, e aprendizagem.
+                      </p>
+                      <p className="mt-6 text-2xl font-medium text-white">
+                        Z - Trabalhar em equipe é unir várias formas de pensar para um só objetivo.
+                      </p>
+                    </div>
+                  </blockquote>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </main>
+        <footer className="bg-gray-50" aria-labelledby="footer-heading">
+          <h2 id="footer-heading" className="sr-only">
+            Footer
+          </h2>
+          <div className="max-w-md mx-auto pt-12 px-4 sm:max-w-7xl sm:px-6 lg:pt-16 lg:px-8">
+            <div className="xl:grid xl:grid-cols-3 xl:gap-8">
+              <div className="space-y-8 xl:col-span-1">
+                <img
+                  className="h-10"
+                  src="https://tailwindui.com/img/logos/workflow-mark.svg?color=gray&shade=300"
+                  alt="Company name"
+                />
+                <p className="text-gray-500 text-base">
+                  Ih foi rápido né? Mas pelo timing chegar até aqui foi oque deu...
+                </p>
+                <p className="text-gray-500 text-base">
+                O que achar de conhecer um pouco mais sobre mim através de minhas redes sociais? só seguir abaixo.
+                </p>
+                <div className="flex space-x-6">
+                  {footerNavigation.social.map((item) => (
+                    <a key={item.name} href={item.href} className="text-gray-400 hover:text-gray-500">
+                      <span className="sr-only">{item.name}</span>
+                      <item.icon className="h-6 w-6" aria-hidden="true" />
+                    </a>
+                  ))}
+                </div>
+              </div>
+            </div>
+            <div className="mt-12 border-t border-gray-200 py-8">
+              <p className="text-base text-gray-400 xl:text-center">&copy; Projeto desenvolvido para o desafio da Raizen Tech Experience.</p>
+            </div>
+          </div>
+        </footer>
+      </div>
+    </div>
   )
 }
 
-export { Header };
+export { Header }
